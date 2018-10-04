f5caa46a5b7fd49a7bc395cb41905d80ec9097d6
ksarapultsev <ksarapultsev2014@gmail.com>
2018-10-04 09:40:14 +0600

    Add fields

--- app/src/main/java/com/kos/work/androidappmoneytracker/MainActivity.java
+++ app/src/main/java/com/kos/work/androidappmoneytracker/MainActivity.java
@@ -2,12 +2,15 @@ package com.kos.work.androidappmoneytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.listview);
     }
 }
